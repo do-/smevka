efb85f210d0e05c86e6bda1d63a656fa1e8b23ce
Dmitry Ovsyanko <do_@pochta.ru>
2021-10-22 21:42:56 +0300

    static xml gone out of js

--- back/lib/Content/ack.js
+++ back/lib/Content/ack.js
@@ -5,16 +5,9 @@ module.exports = {
 do_reply_to_ack:
 
     function () {
-
-		return (
-`<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
-  <SOAP-ENV:Header/>
-  <SOAP-ENV:Body>
-	<ns4:AckResponse xmlns:ns4="urn://x-artefacts-smev-gov-ru/services/message-exchange/types/1.1" xmlns:ns2="urn://x-artefacts-smev-gov-ru/services/message-exchange/types/basic/1.1" xmlns:ns3="urn://x-artefacts-smev-gov-ru/services/message-exchange/types/faults/1.1"/>
-  </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>`
-		)
-
+    
+		return require ('fs').readFileSync ('./Static/ack.xml')
+    
     },
         
 }
