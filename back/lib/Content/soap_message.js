208999e81cd3b2066c468e7b0906d7eb4f508801
Dmitry Ovsyanko <do_@pochta.ru>
2021-10-26 21:50:56 +0300

    xml to json is back to server, now sax based

--- back/lib/Content/soap_message.js
+++ back/lib/Content/soap_message.js
@@ -1,4 +1,3 @@
-const {Readable} = require ('stream')
 const Saxophone = require ('saxophone')
 
 module.exports = {
@@ -48,5 +47,82 @@ do_parse_soap_message:
     	})
     	
 	},
+	
+////////////////////////////////////////////////////////////////////////////////
+
+get_json_of_soap_message:
+
+	async function () {
+	
+    	let {rq: {xml}} = this, stack = [], m2o = map => Object.fromEntries (map.entries ())
+		
+		let on = () => stack.push ([new Map (), '']); on ()
+		
+		let app = value => {
+		
+			value = value.replace (/\s+/g, ' ').trim (); if (!value) return
+			
+			stack [stack.length - 1] [1] += value
+			
+		}
+		
+		let set = (key, value) => {
+			
+			if (/^x(mlns|si:)\b/.test (key)) return
+			
+			let pos = key.indexOf (':'); if (pos > -1) key = key.slice (pos + 1)
+
+			let [map] = stack [stack.length - 1]
+
+			if (!map.has (key)) return map.set (key, value)
+
+			let box = map.get (key); if (!Array.isArray (box)) map.set (key, box = [box])
+	
+			box.push (value)
+
+ 		}
+		
+		let off = name => {
+		
+			let [map, txt] = stack.pop ()
+			
+			set (name, map.size ? m2o (map) : txt)
+		
+		}				
+		
+		await new Promise ((ok, fail) => new Saxophone ().on ('error', fail).on ('finish', ok)			
+			.on ('text',     o => app (o.contents))
+			.on ('tagclose', tag => off (tag.name))
+			.on ('tagopen',  tag => {
+				on ()
+				for (let [k, v] of Object.entries (Saxophone.parseAttrs (tag.attrs))) set (k, v)
+				if (tag.isSelfClosing) off (tag.name)
+			})
+			.parse (xml)
+		)
+		
+		let result = m2o (stack [0] [0])
+		
+		let get_message_primary_content = o => {
+
+			if (typeof o != 'object') return null
+
+			let e = Object.entries (o); if (!e.length) return null
+
+			for (let [k, v] of e) {
+
+				if (k == 'MessagePrimaryContent') return v
+
+				let c = get_message_primary_content (v); if (c) return c
+
+			}
+
+			return null
+
+		}
+		
+		return get_message_primary_content (result)
+	
+	}
         
 }
--- front/root/_/js.js
+++ front/root/_/js.js
@@ -30,75 +30,6 @@ function say (s) {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-function get_message_primary_content (o) {
-
-	if (typeof o != 'object') return null
-
-	let e = Object.entries (o); if (!e.length) return null
-	
-	for (let [k, v] of e) {
-
-		if (k == 'MessagePrimaryContent') return v
-		
-		let c = get_message_primary_content (v); if (c) return c
-	
-	}
-	
-	return null
-
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-function el_to_obj (el) {
-
-	let o = {}, txt = ''
-
-	let {attributes, childNodes} = el
-
-	for (let i = 0; i < attributes.length; i ++) {
-		
-		let a = attributes.item (i)
-		
-		if (/^x(mlns|si)/.test (a.name)) continue
-
-		o [a.localName] = a.value
-		
-	}
-
-	for (let i = 0; i < childNodes.length; i ++) {
-		
-		let n = childNodes.item (i); switch (n.nodeType) {
-		
-			case Node.TEXT_NODE:
-
-				txt += n.wholeText.replace (/\s+/g, ' ').trim ()
-
-				break
-				
-			case Node.ELEMENT_NODE:
-
-				let [[k, v]] = Object.entries (el_to_obj (n))
-
-				if (!(k in o)) {
-					o [k] = v
-				}
-				else {
-					if (!Array.isArray (o [k])) o [k] = [o [k]]
-					o [k].push (v)
-				}
-				break
-
-		}
-		
-	}
-
-	return {[el.localName]: Object.keys (o).length == 0 ? txt : o}
-
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 function refresh () {
 
 	let txt = get_textarea ().value
@@ -111,12 +42,10 @@ function refresh () {
 
 	}
 	else {
-
-		let o = el_to_obj (doc.documentElement)
+	
+		let [form] = document.querySelectorAll ('form')
 		
-		let c = get_message_primary_content (o)
-
-		say (JSON.stringify (c, null, 2))
+		form.submit ()
 
 	}
 
