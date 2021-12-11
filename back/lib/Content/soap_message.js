663214c088f00762620e54e9e28b038cc7a4c29b
Dmitry Ovsyanko <do_@pochta.ru>
2021-12-11 20:11:18 +0300

    soap_message: xml-toolkit instead of saxophone-object-stream

--- back/lib/Content/soap_message.js
+++ back/lib/Content/soap_message.js
@@ -1,5 +1,4 @@
-const Saxophone = require ('saxophone')
-const {XMLReader} = require ('saxophone-object-stream')
+const {XMLReader, SAXEvent, XMLLexer, MoxyLikeJsonEncoder} = require ('xml-toolkit')
 
 module.exports = {
 	
@@ -8,9 +7,36 @@ module.exports = {
 get_json_of_soap_message:
 
 	async function () {
-
-		return XMLReader.get (this.body, {localName : 'MessagePrimaryContent'})
-			
-	}	
 	
+		const lex = new XMLLexer ({})
+
+		const sax = new XMLReader ({
+			stripSpace : true,
+			collect    : e => true,
+			find       : e => e.localName === 'MessagePrimaryContent' && e.type === SAXEvent.TYPES.END_ELEMENT,
+			map        : MoxyLikeJsonEncoder ()
+		})
+
+		lex.pipe (sax)
+
+		return new Promise ((ok, fail) => {
+		
+			lex.on ('error', fail)
+			sax.on ('error', fail)
+			
+			const not_found = () => fail (new Error ('MessagePrimaryContent not found'))
+			
+			sax.on ('end', not_found)
+
+			sax.on ('data', data => {
+				sax.off ('end', not_found)
+				ok (data)
+			})
+			
+			lex.end (this.body)
+		
+		})
+		
+	}	
+
 }
