88cf2c339eab082508efa56d0ee774ad756faae4
Dmitry Ovsyanko <do_@pochta.ru>
2022-02-20 13:00:53 +0300

    calling get_request_of_{type}

--- back/lib/Content/send_request.js
+++ back/lib/Content/send_request.js
@@ -6,21 +6,19 @@ do_register_send_request:
 
     async function () {
 
-    	let {last, body} = this
-
-    	last.id = ZERO_UUID
+    	let {last, data} = this
     	
-    	let [[k, v]] = Object.entries (JSON.parse (body))
+    	let [[k, v]] = Object.entries (data)
 
-    	last.data = v
-
-    	last.type = k
+    	const type = last.type = k
 
 			.replace (/Re(quest|sponse)$/, '') // method, not message name
 
 			.replace (/[A-Z]/g,                // CamelCase to under_scores
 				(m, o) => (o ? '_' : '') + m.toLowerCase ()
 			)
+			
+		last.data = await this.fork ({type, part: 'request'}, {data: v})
 
 		return {}
 
@@ -37,11 +35,11 @@ do_reply_to_send_request:
 
     	let {MessageID, MessagePrimaryContent} = await this.get_body_element ('SenderProvidedRequestData')
     	
-    	this.body = JSON.stringify (MessagePrimaryContent)
+    	last.id = MessageID
+
+    	this.data = MessagePrimaryContent
     	
     	await this.call ('do_register_send_request')
-
-    	last.id = MessageID
 
 		return require ('fs').readFileSync ('./Static/send.xml')
 
