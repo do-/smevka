4b1967504c8b0a800fb924195aac0e78aad351a7
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-03 12:00:11 +0300

    do_reply_to_ack: body_document instead of get_body_element

--- back/lib/Content/ack.js
+++ back/lib/Content/ack.js
@@ -5,24 +5,24 @@ module.exports = {
 do_reply_to_ack:
 
     async function () {
-    	
-    	const {last, body, rq} = this
-    	
-    	if (body) {
-    	
-    		const uuid = this.get_body_element ('AckTargetMessage')
-    		
+
+    	const {last, body_document, rq} = this
+
+    	if (body_document) {
+
+    		const uuid = body_document.Body.AckRequest.AckTargetMessage
+
     		if (uuid) for (const [k, v] of last.entries ()) if (v.uuid == uuid) last.delete (k)
-    	
+
     	}
     	else if (rq.id) {
-    	
+
     		last.delete (rq.id)
-    	
+
     	}
-    
+
 		return require ('fs').readFileSync ('./Static/ack.xml')
-    
+
     },
         
 }
