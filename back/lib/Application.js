da19f19a340acdd8999f9c03ec5071609f617949
Dmitry Ovsyanko <do_@pochta.ru>
2023-01-04 16:55:53 +0300

    doix ConsoleLogger + tz

--- back/lib/Application.js
+++ back/lib/Application.js
@@ -8,15 +8,35 @@ const MockService = require ('./MockService.js')
 const xs_smev = new XMLSchemata (Path.join (__dirname, 'Static', 'smev-message-exchange-service-1.1.xsd'))
 
 
+
+
+
+
+
+
+
+
+	const off = (new Date ()).getTimezoneOffset (), lag = off * 60000
+
+	const TZ_HH_MM =
+		(off > 0 ? '-' : '+') +
+		(new Date (2000, 1, 1, 0, -2 * off, 0))
+			.toJSON ()
+			.slice (11, 16)
+
+	
+
 class ConsoleLogger {
 
 	log ({message, level}) {
 	
-		console.log ([
-			new Date ().toISOString(), 
-			level || 'info',
-			message
-		].join (' ').trim ())
+		let s = (new Date (Date.now () - lag)).toISOString ().slice (0, 23) + TZ_HH_MM
+
+		s += ' ' + (level || 'info')
+		
+		if (message) s += ' ' + message
+	
+		console.log (s)
 
 	}
 
@@ -24,72 +44,83 @@ class ConsoleLogger {
 
 class EventLogger {
 
-	constructor (job) {
+	constructor (emitter) {
 
 		this.now = Date.now ()
-
-		this.job = job
-		
-		this.logger = job.logger
 
 		for (const name of Object.getOwnPropertyNames (Object.getPrototypeOf (this))) 
 
 			if (name.slice (-7) === 'Message')
 
-				job.on (name.slice (0, -7), (j, p) => 
+				emitter.on (name.slice (0, -7), (j, p) => 
 
 					this.logger.log (this [name] (p))
 
 				)
 
 	}
-
-	getPrefix () {
 	
-		let s = '', j = this.job
-		
-		while (j) {
-			
-			s = !s ? j.uuid : j.uuid + '/' + s
-
-			j = j.parent
-			
-		}
-
-		return s
-
-	}
-
 	message (message, level = 'info') {
 	
-		const pre = this.getPrefix ()
+		const {prefix} = this
 	
-		if (pre) message = pre + ' ' + message
+		if (prefix) message = prefix + ' ' + message
 		
 		return {level, message}
 		
 	}
+	
+}
+
+class JobEventLogger extends EventLogger {
+
+	constructor (job) {
+	
+		super (job)
+
+		this.now = Date.now ()
+
+		this.job = job
+		
+		this.logger = job.logger
+			
+	}
+	
+	get prefix () {
+
+		let j = this.job, p = j.uuid
+		
+		while (j = j.parent) p = j.uuid + '/' + p
+		
+		return p
+
+	}
 		
 	startMessage () {
+	
 		return this.message ('>')
+		
 	}
 
 	methodMessage (m) {
+	
 		return this.message (m + ' ' + JSON.stringify (this.job.rq))
+		
 	}
 
-	errorMessage (m) {
+	errorMessage () {
+	
 		return this.message ('' + this.job.error, 'error')
+		
 	}
 
 	finishMessage () {
+
 		return this.message ('< ' + (Date.now () - this.now) + ' ms')
+
 	}
 
 }
-
-
-
 
 module.exports = class extends LegacyApplication {
 
@@ -105,7 +136,7 @@ module.exports = class extends LegacyApplication {
 			},
 			
 			generators: {			
-				eventLogger: job => new EventLogger (job),
+				eventLogger: job => new JobEventLogger (job),
 			},
 
 			modules: {
